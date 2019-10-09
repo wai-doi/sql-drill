0f0baf77d909e86b624117b6a2438ccf34bf6313
wai-doi <yusuke.doi@esm.co.jp>
2019-10-09 17:36:57 +0900

    2-2

---
+++ chapter_2/2.sql
@@ -0,0 +1,47 @@
+SELECT
+  ProductName
+, Price
+FROM
+  Products
+;
+
+SELECT
+  CustomerName
+, Address
+FROM
+  Customers
+;
+
+SELECT
+  ProductID
+, ProductName
+, Price
+FROM
+  Products
+;
+
+SELECT
+  EmployeeName
+, Email
+, Height
+FROM
+  Employees
+;
+
+
+SELECT
+  CustomerCode
+, CustomerName
+, CustomerCode
+FROM
+  Customers
+;
+
+SELECT
+  ProductCode
+, Prcice
+, ProductName
+, ProductCode
+FROM
+  Products
+;
