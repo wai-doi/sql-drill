c952ca94d8b68ee4513c15461bec9116fddbc92d
wai-doi <yusuke.doi@esm.co.jp>
2019-10-09 17:48:20 +0900

    2-3

---
+++ chapter_2/3.sql
@@ -0,0 +1,42 @@
+SELECT
+  ProductName AS 商品名
+, Price AS 単価
+FROM
+  Products
+;
+
+SELECT
+  EmployeeName AS 社員名
+FROM
+  Employees
+;
+
+SELECT
+  CustomerCode AS 顧客コード
+, CustomerName AS 顧客名
+FROM
+  Customers
+;
+
+SELECT
+  ProductCode AS 商品コード
+, ProductName AS 商品名
+, Price AS 価格
+FROM
+  Products
+;
+
+SELECT
+  CustomerName AS 顧客名
+, CustomerName AS 得意先名
+FROM
+  Customers
+;
+
+SELECT
+  EmployeeName AS 社員名
+, Email AS メールアドレス
+, Email AS 連絡先
+FROM
+  Employees
+;
